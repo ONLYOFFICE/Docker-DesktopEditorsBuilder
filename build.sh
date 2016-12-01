ce97f1a82e17bbca62723cbfb157b2919d926311
Alexey Golubev <alexey.golubev@onlyoffice.com>
2016-12-01 14:10:38 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BUILD_DIR=/var/lib/onlyoffice
+MODULES=(core:all sdkjs:all server:all)
+
+if [ ! $# -eq 0 ]; then
+  MODULES=($@)
+fi
+
+for i in ${MODULES[@]}; do
+  OLD_IFS="${IFS}"
+  IFS=':' read -r -a MODULE <<< ${i}
+  IFS="${OLD_IFS}"
+  if [ ${MODULE[0]} = "core" ]; then
+    cd $BUILD_DIR/core/Common/3dParty && ./make.sh
+  fi
+  cd $BUILD_DIR/${MODULE[0]} && make ${MODULE[@]:1}
+done
